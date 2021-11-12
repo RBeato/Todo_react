b99909f07c1a0e07d97fae40c48ba9736ad191ad
Romeu Beato <romeufbeato@gmail.com>
2021-11-12 16:41:26 +0000

    second

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,19 @@
 import PropTypes from 'prop-types'
+import { useLocation } from 'react-router-dom'
 import Button from './Button'
 
 
-const Header = ({title})=>{
-    return (
+const Header = ({title, onAdd, showAdd})=>{
+    const location = useLocation()
+
+   return (
         <header className='header'>
             <h1 >{title}</h1>
-            <Button color='green' text='Add' />
+            {location.pathname === '/' && <Button 
+            color = {showAdd? 'red':'green'} 
+            text={showAdd? 'Close':'Add'}
+            onClick= { onAdd } 
+            />}
         </header>
     )
 }
